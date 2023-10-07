129f089b05c712b60e69d383e29ba8e6a692445c
Valeriy Kovalenko <57315743+valeriyFrontend@users.noreply.github.com>
2023-10-07 16:55:34 +0300

    Update hideCursor.js

--- src/hideCursor.js
+++ src/hideCursor.js
@@ -3,5 +3,5 @@ const isPanelVisible = document.querySelector('.player--panel-visible');
 if(!isPanelVisible) {
   document.querySelector("body").style.cursor = "none";
 } else {
-  document.querySelector("body").style.cursor = "inherit";
+  document.querySelector("body").style.cursor = "default";
 }
