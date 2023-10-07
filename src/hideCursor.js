e88f9ccc9f4ec15c2ae693e10eb11a4b3d8c9e7e
Valeriy Kovalenko <57315743+valeriyFrontend@users.noreply.github.com>
2023-10-07 17:17:31 +0300

    Update hideCursor.js

--- src/hideCursor.js
+++ src/hideCursor.js
@@ -1,8 +1,12 @@
-// Function to handle changes in visibility
-function handleVisibilityChange(mutationsList, observer) {
-  for (const mutation of mutationsList) {
+const isPanelVisible = document.querySelector('.player--panel-visible');
+
+const options = {
+  attributes: true
+}
+
+function callback(mutationList, observer) {
+  mutationList.forEach(function(mutation) {
     if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-      const isPanelVisible = document.querySelector('.player--panel-visible');
       if (!isPanelVisible) {
         console.log(isPanelVisible, 1);
         document.querySelector("body").style.cursor = "none";
@@ -11,17 +15,8 @@ function handleVisibilityChange(mutationsList, observer) {
         document.querySelector("body").style.cursor = "default";
       }
     }
-  }
+  })
 }
 
-// Select the element you want to observe for changes
-const targetNode = document.querySelector('.player--panel-visible');
-
-// Create a MutationObserver
-const observer = new MutationObserver(handleVisibilityChange);
-
-// Configure the observer to watch for changes in attributes
-const config = { attributes: true, attributeFilter: ['class'] };
-
-// Start observing the target node
-observer.observe(targetNode, config);
+const observer = new MutationObserver(callback)
+observer.observe(isPanelVisible, options)
